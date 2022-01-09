8ddbc00d43903e8deb870730acebae865740e63d
otarhe <78844558+otarhe@users.noreply.github.com>
2022-01-10 00:05:32 +0100

    Update pressable_api_connection_web.php

--- pressable_api_connection_web.php
+++ pressable_api_connection_web.php
@@ -53,7 +53,7 @@ curl_setopt_array($curl, array(
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  //You can change the request type from here
+  //You can chnage the request method from GET to POST or any method
   CURLOPT_CUSTOMREQUEST => "GET",
   CURLOPT_HTTPHEADER => array(
     $b  . $token,     
