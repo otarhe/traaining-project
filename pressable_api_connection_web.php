868e59bef7bc1c1a55cde99788e793df34c0a2a9
tarhe <78844558+otarhe@users.noreply.github.com>
2023-01-05 19:18:34 +0100

    Updated the code to add form
    Added html input form to collect client ID and Secret

--- pressable_api_connection_web.php
+++ pressable_api_connection_web.php
@@ -1,36 +1,50 @@
+<form method="post">
+  <label for="client_id">Client ID:</label><br>
+  <input type="text" id="client_id" name="client_id"><br>
+  <label for="client_secret">Client Secret:</label><br>
+  <input type="text" id="client_secret" name="client_secret"><br>
+  <input type="submit" value="Submit">
+</form> 
+
 <?php
 
 /*
 Plugin Name: Pressable API connection web application
-Description: Demonstrating how to access the Pressable API using a simple web application
+Description: Demonstrating how to access the Pressable API using a simple web application to list all website on your Pressable account
 Plugin URI:  https://pressable.com/
 Author:      Obatarhe Otughwor
-Version:     1.0
+Version:     2.0
 */
 
 
 $curl = curl_init();
-$auth_data = array(
-  'client_id'     => 'ADD-CLIENT-ID-HERE',
-  'client_secret'   => 'ADD-CLIENT-SECRET-HERE',
-  'grant_type'    => 'client_credentials'
-);
-curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $auth_data);
-curl_setopt($curl, CURLOPT_URL, 'https://my.pressable.com/auth/token');
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-$results = curl_exec($curl);
-if(!$results){die("Connection Failure");}
-curl_close($curl);
 
+// Check if the form has been submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $client_id = $_POST['client_id'];
+  $client_secret = $_POST['client_secret'];
+
+  $auth_data = array(
+    'client_id'     => $client_id,
+    'client_secret'   => $client_secret,
+    'grant_type'    => 'client_credentials'
+  );
+
+  curl_setopt($curl, CURLOPT_POST, 1);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $auth_data);
+  curl_setopt($curl, CURLOPT_URL, 'https://my.pressable.com/auth/token');
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+  $results = curl_exec($curl);
+  if(!$results){die("Connection Failure");}
+  curl_close($curl);
+}
 
 $results = json_decode($results, true);
 
- echo "Bearer token:" . '<br/>';
- $res = print_r($results["access_token"]);
+echo "Bearer token:" . '<br/>';
+$res = print_r($results["access_token"]);
 
- 
 
 
 echo $res;
